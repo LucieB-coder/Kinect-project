0940d4f60bfae9bc54cc2bd21331a26446a2955a
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-01-11 11:15:23 +0100

    ✨ Add view with kinect connection

--- source/TP1/MainWindow.xaml.cs
+++ source/TP1/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -16,13 +19,55 @@ using System.Windows.Shapes;
 namespace TP1
 {
     /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
+    /// Logique d'interaction pour MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor kinectSensor = null;
+
         public MainWindow()
         {
-            InitializeComponent();
+            
+
+            // get the kinectSensor object
+            this.kinectSensor = KinectSensor.GetDefault();
+
+            // set IsAvailableChanged event notifier
+            this.kinectSensor.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+
+            // open the sensor
+            this.kinectSensor.Open();
+
+            // initialize the components (controls) of the window
+            this.InitializeComponent();
+
+            // set the status text
+            this.StatusText.Text = this.kinectSensor.IsAvailable ? "yeepee" : "oh no";
+
+            // use the window object as the view model in this simple example
+            this.DataContext = this;
+
+            
+
+
+           
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Handles the event which the sensor becomes unavailable (E.g. paused, closed, unplugged).
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            // on failure, set the status text
+            this.StatusText.Text = this.kinectSensor.IsAvailable ? "yeepee" : "oh no";
+        }
+
     }
 }
