3f3ae814f33d819817f54e410f0e7c66cf34b0cc
lucie.bedouret <lucie.bedouret@etu.uca.fr>
2024-01-31 08:45:04 +0100

    :sparkles: BaseGesture and Posture classes

---
+++ source/KinectUtils/BaseGesture.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+
+namespace KinectUtils
+{
+    public abstract class BaseGesture
+    {
+        public EventHandler<GestureRecognizedEventArgs> GestureRecognized { get; set; }
+        public String GestureName { get; set; }
+
+        public BaseGesture(EventHandler<GestureRecognizedEventArgs> gestureRecognized, string gestureName)
+        {
+            GestureRecognized = gestureRecognized;
+            GestureName = gestureName;
+        }
+
+        public abstract void TestGesture(Body body);
+        protected void OnGestureRecognized()
+        {
+            GestureRecognized?.Invoke(this, new GestureRecognizedEventArgs(GestureName));
+        }
+    }
+
+    internal delegate void GestureRecognizedEventHandler(object sender, GestureRecognizedEventArgs e);
+    public class GestureRecognizedEventArgs : EventArgs
+    {
+        public String GestureName { get; set; }
+
+        public GestureRecognizedEventArgs( String gestureName )
+        {
+            GestureName = gestureName;
+        }
+    }
+}
---
+++ source/KinectUtils/Posture.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectUtils
+{
+    public abstract class Posture : BaseGesture
+    {
+        public Posture(EventHandler<GestureRecognizedEventArgs> gestureRecognized, string gestureName) : base(gestureRecognized, gestureName)
+        {
+        }
+        protected abstract bool TestPosture(Body body);
+    }
+}
